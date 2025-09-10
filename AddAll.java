474d92a566b1d5cbc107b19de5d5524023528759
fazlerabbi13 <mohammad0132794@gmail.com>
2025-09-10 20:13:23 +0600

    Creating an ArrayList and adding initial elements.

---
+++ AddAll.java
@@ -0,0 +1,24 @@
+// Java program to demonstrate 
+// adding all elements to the end of a list
+import java.util.ArrayList;
+public class AddAll {
+  
+    public static void main(String[] args) {
+      
+        // Creating an ArrayList and 
+        // adding initial elements
+        ArrayList<String> l1 = new ArrayList<>();
+        l1.add("Java");
+        l1.add("C++");
+
+        // Creating another ArrayList 
+        // with elements to be added
+        // ArrayList<String> l2 = new ArrayList<>();
+        // l2.add("C");
+
+        // // Adding all elements 
+        // // from l2 to l1
+        // l1.addAll(l2);
+        // System.out.println("" + l1);
+    }
+}
--- tempCodeRunnerFile.java
+++ tempCodeRunnerFile.java
@@ -1,1 +1,7 @@
-Updating value at index 0
+  // ArrayList<String> l2 = new ArrayList<>();
+        // l2.add("C");
+
+        // // Adding all elements 
+        // // from l2 to l1
+        // l1.addAll(l2);
+        // System.out.println("" + l1);
