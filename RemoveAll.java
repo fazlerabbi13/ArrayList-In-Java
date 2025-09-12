2dd4247f5336febecae4b82c69bbc67ce8af2227
fazlerabbi13 <mohammad0132794@gmail.com>
2025-09-12 21:18:38 +0600

    Creating an ArrayList of numbers.

---
+++ RemoveAll.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+
+public class RemoveAll {
+     public static void main(String[] args) {
+      
+        // Creating an ArrayList of numbers
+        ArrayList<Integer> n1 = new ArrayList<>();
+        n1.add(1);
+        n1.add(2);
+        n1.add(3);
+        n1.add(4);
+        n1.add(5);
+
+        System.out.println("Original list: " + n1);
+
+        // Creating another ArrayList 
+        // with elements to remove
+        // ArrayList<Integer> n2 = new ArrayList<>();
+        // n2.add(1);
+        // n2.add(2);
+        // n2.add(3);
+
+        // Removing specified elements 
+        // using removeAll()
+        // n1.removeAll(n2);
+
+        // System.out.println("List after removing specific elements: " + n1);
+    }
+}
