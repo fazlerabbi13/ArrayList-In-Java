887e19462cda48c696cd0b33c32a7ea8841df990
fazlerabbi13 <mohammad0132794@gmail.com>
2025-10-25 18:57:09 +0600

    calculate average mark of all students.

--- Exercises/AverageMarks.java
+++ Exercises/AverageMarks.java
@@ -5,6 +5,19 @@ public class AverageMarks {
     public static void main(String[] args) {
         ArrayList<Integer> marks = new ArrayList<>();
 
-        marks.add(e)
+        marks.add(75);
+        marks.add(85);
+        marks.add(65);
+        marks.add(72);
+        marks.add(70);
+
+        int sum = 0;
+
+        for(int mark:marks){
+            sum +=mark;
+        }
+        double average = (double)sum / marks.size();
+
+        System.out.println(average);
     }
 }
